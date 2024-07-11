56cf0c3e667da2498f03be586f8d24f69c3dc602
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-11 01:56:00 -0400

    chore: Add form to homepage for user input

--- basics/app.js
+++ basics/app.js
@@ -3,18 +3,25 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
-  console.log(url, method)
-
   // set header metadata to html (says we are sending html); there are many headers
   // some headers are set automatically by the server
   res.setHeader('Content-Type', 'text/html')
 
-  // writes/sends back chunks of data
-  res.write('<html>')
-  res.write('<head><title>My first page</title></head>')
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
-  res.write('</html>')
-  res.end()
+  if (url === '/') {
+    // writes/sends back chunks of data
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write(
+      `<body>
+        <form action='/message' method='POST'>
+          <input type='text' name='message'/>
+          <button type='submit'>Submit</button>
+        </form>
+      </body>`
+    )
+    res.write('</html>')
+    return res.end()
+  }
 }) // creates a server
 
 server.listen(3000)
