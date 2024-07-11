7a756685da771ce1a160560e6c22fa175742e3dd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-11 05:17:23 -0400

    Update imports

--- basics/http-server/http-server.js
+++ basics/http-server/http-server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 // route handlers
-const routes = require('./routes')
+const { routes } = require('./routes')
 
 const server = http.createServer(routes) // creates a server
 
