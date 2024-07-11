1660b3ba3e7e2d150aec70762f751ebc6bd38d3e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-11 10:46:53 -0400

    chore: Add server.js file with basic Express setup

---
+++ shop/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('colors')
+
+const PORT = 3000
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.url)
+  next()
+})
+
+// routes
+app.get('/', (req, res) => {
+  res.status(200)
+  return res.send('<h1>Hello, world!</h1>')
+})
+
+// server
+app.listen(PORT, (req, res) => {
+  console.log(`SERVER RUNNING ON PORT: ${PORT}`.green.inverse)
+})
