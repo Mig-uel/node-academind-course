527eaa200701ddf929297b9fd365a7efc0a9942b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-13 11:16:22 -0400

    chore: Update admin route to use '/admin/product' endpoint

--- shop/routes/admin.routes.js
+++ shop/routes/admin.routes.js
@@ -1,19 +1,20 @@
 const adminRouter = require('express').Router()
 
-adminRouter.route('/add-product').get((req, res) => {
-  return res.status(200).send(
-    `<form action="/admin/product" method="POST">
+adminRouter
+  .route('/product')
+  .get((req, res) => {
+    return res.status(200).send(
+      `<form action="/admin/product" method="POST">
         <input type="text" name="title" required/>
         <button type="submit">Add Product</button>
       </form>`
-  )
-})
+    )
+  })
+  .post((req, res) => {
+    const { body } = req
+    console.log(body)
 
-adminRouter.route('/product').post((req, res) => {
-  const { body } = req
-  console.log(body)
-
-  return res.redirect(302, '/')
-})
+    return res.send('<h1>Product Added!</h1>')
+  })
 
 module.exports = { adminRouter }
