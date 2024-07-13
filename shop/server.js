1bb39751c0c1a5ef00bbfcb39d1ce62ae09b7b1f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-13 10:21:29 -0400

    chore: Refactor server.js for improved routing and middleware handling

--- shop/server.js
+++ shop/server.js
@@ -1,25 +1,28 @@
 const express = require('express')
 require('colors')
 
+// router
+const { adminRouter } = require('./routes/admin.routes')
+const { shopRouter } = require('./routes/shop.routes')
+
+// config
 const PORT = 3000
 const app = express()
 
 // middleware
 app.use((req, res, next) => {
-  console.log(req.url)
+  console.log(`[${req.method}] - ${req.url} - ${res.statusCode}`.yellow.inverse)
   next()
-})
+}) // method - url - status
+app.use(express.urlencoded({ extended: true })) // parse form data
+app.use(express.json()) // parse json data
 
 // routes
-app.get('/', (req, res) => {
-  res.status(200)
-  return res.send('<h1>Hello, world!</h1>')
-})
+app.use(adminRouter)
+app.use(shopRouter)
 
-app.use((req, res, next) => {
-  res.status(404)
-  return res.send('<h1>Path not found</h1>')
-})
+// 404 - not found
+app.use((req, res, next) => res.redirect(302, '/'))
 
 // server
 app.listen(PORT, (req, res) => {
