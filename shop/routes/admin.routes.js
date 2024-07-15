6e7817a27e201237757ffc105a17865f39b4256d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-14 20:55:18 -0400

    refactor: Update admin.routes.js for better code organization
    This commit refactors the admin.routes.js file in the shop/routes directory to improve code organization. The changes include removing unused code related to product handling and restructuring the code to use separate controller functions for handling the add product form and product addition. This refactoring enhances the maintainability and readability of the codebase.

--- shop/routes/admin.routes.js
+++ shop/routes/admin.routes.js
@@ -1,25 +1,11 @@
 const path = require('path')
+
+const {
+  getAddProductForm,
+  addProduct,
+} = require('../controllers/admin.controller')
 const adminRouter = require('express').Router()
 
-const products = []
+adminRouter.route('/product').get(getAddProductForm).post(addProduct)
 
-adminRouter
-  .route('/product')
-  .get((req, res) => {
-    return res
-      .status(200)
-      .render('add-product', {
-        docTitle: 'Add Product',
-        path: '/admin/product',
-      })
-  })
-  .post((req, res) => {
-    const { body } = req
-    const { title } = body
-
-    products.push({ title })
-
-    return res.redirect('/')
-  })
-
-module.exports = { adminRouter, products }
+module.exports = { adminRouter }
