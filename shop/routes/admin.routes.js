088676c42957a970d253fa716789d2c2415606f0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-18 19:24:14 -0400

    feat: Add route for editing products in admin panel
    The code changes in this commit add a new route in the admin.routes.js file for editing products. The route is "/edit/:id" and it calls the getEditProductForm function in the admin.controller.js file. This enhancement improves the admin functionality by allowing the user to edit existing products directly from the admin panel.

--- shop/routes/admin.routes.js
+++ shop/routes/admin.routes.js
@@ -4,10 +4,12 @@ const {
   getAddProductForm,
   addProduct,
   adminGetProducts,
+  getEditProductForm,
 } = require('../controllers/admin.controller')
 const adminRouter = require('express').Router()
 
 adminRouter.route('/products').get(adminGetProducts)
 adminRouter.route('/products/add').get(getAddProductForm).post(addProduct)
+adminRouter.route('/edit/:id').get(getEditProductForm)
 
 module.exports = { adminRouter }
