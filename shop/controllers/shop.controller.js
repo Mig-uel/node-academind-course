fe64eaad3d14d7c8816b58f479be1b64e72e9665
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-18 11:32:22 -0400

    refactor: Use Product model for fetching products in shop.controller.js
    This commit updates the shop.controller.js file in the shop/controllers directory to use the Product model for fetching products. Instead of directly accessing the products array, the Product.fetchAll() function is now used to retrieve the products from the database. This change adheres to the MVC pattern and improves the maintainability and scalability of the project.

--- shop/controllers/shop.controller.js
+++ shop/controllers/shop.controller.js
@@ -1,9 +1,9 @@
 const { Product } = require('../models/product.model')
 
 const getProducts = async (req, res) => {
-  const products = Product.fetchAll()
-  
-  return res.render('shop', { products, docTitle: 'Shop', path: '/' })
+  Product.fetchAll((products) => {
+    return res.render('shop', { products, docTitle: 'Shop', path: '/' })
+  })
 }
 
 module.exports = { getProducts }
