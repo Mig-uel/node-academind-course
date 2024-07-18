a4cbd625b484a054474590744d5a35039fbc3ba4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-18 13:42:24 -0400

    refactor: Update Product model to use getProductsFromFile function for saving products
    The Product model in the product.model.js file has been updated to use the getProductsFromFile function for saving products. This change improves the codebase by separating the file system operations into a separate function, enhancing modularity and maintainability.

--- shop/models/product.model.js
+++ shop/models/product.model.js
@@ -1,35 +1,33 @@
 const fs = require('fs')
 const path = require('path')
 
-class Product {
-  static pathname = path.join(__dirname, '..', 'data', 'products.json')
+const pathName = path.join(__dirname, '..', 'data', 'products.json')
 
+const getProductsFromFile = (callback) => {
+  fs.readFile(pathName, (error, data) => {
+    if (error) callback([])
+    else {
+      callback(JSON.parse(data))
+    }
+  })
+}
+
+class Product {
   constructor(title) {
     this.title = title
   }
 
   save() {
-    fs.readFile(Product.pathname, (err, data) => {
-      let products = []
-
-      if (!err) {
-        products = JSON.parse(data)
-      }
-
+    getProductsFromFile((products) => {
       products.push(this)
-
-      fs.writeFile(Product.pathname, JSON.stringify(products), (err) => {
+      fs.writeFile(pathName, JSON.stringify(products), (err) =>
         console.log(err)
-      })
+      )
     })
   }
 
   static fetchAll(callback) {
-    fs.readFile(Product.pathname, (err, data) => {
-      if (err) callback([])
-
-      callback(JSON.parse(data))
-    })
+    getProductsFromFile(callback)
   }
 }
 
