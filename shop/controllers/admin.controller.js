e1ca3e2372b8b0bb856b760f92ba6c5d9e6219c6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-18 11:09:23 -0400

    refactor: Update admin.controller.js to use Product model for adding products
    This commit updates the admin.controller.js file in the shop/controllers directory to use the Product model from the product.model.js file for adding products. Instead of directly manipulating the products array, a new instance of the Product model is created with the provided title and saved to the database. This change improves the codebase by adhering to the MVC pattern and enhances the maintainability and scalability of the project.

--- shop/controllers/admin.controller.js
+++ shop/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-const products = []
+const { Product } = require('../models/product.model')
 
 const getAddProductForm = async (req, res) => {
   return res.status(200).render('add-product', {
@@ -11,9 +11,10 @@ const addProduct = async (req, res) => {
   const { body } = req
   const { title } = body
 
-  products.push({ title })
+  const product = new Product(title)
+  product.save()
 
   return res.redirect('/')
 }
 
-module.exports = { getAddProductForm, addProduct, products }
+module.exports = { getAddProductForm, addProduct }
