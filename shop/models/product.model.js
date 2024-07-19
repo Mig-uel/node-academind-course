42a3403a6b2054226c11caec0b8b3eeb38b9aa1f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-19 12:18:05 -0400

    refactor: Update Product model to use Sequelize
    This commit refactors the Product model in the shop application to use Sequelize, a popular ORM library for Node.js. The changes include defining the model using the Sequelize.define method and specifying the data types and constraints for each attribute. This improvement enhances the maintainability and scalability of the codebase by leveraging the powerful features provided by Sequelize for database operations.

--- shop/models/product.model.js
+++ shop/models/product.model.js
@@ -1,61 +1,29 @@
-const { db } = require('../utils/db.utils')
-const { Cart } = require('../models/cart.models')
+const { Sequelize, DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/db.utils')
 
-class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title
-    this.imageUrl = imageUrl
-    this.description = description
-    this.price = price
-  }
+const Product = sequelize.define('product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
 
-  async save() {
-    try {
-      const sql =
-        'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)'
-
-      const res = await db.execute(queryString, [
-        this.title,
-        this.price,
-        this.imageUrl,
-        this.description,
-      ])
-
-      if (!res) throw new Error()
-
-      return res
-    } catch (error) {
-      return error.message
-    }
-  }
-
-  static async fetchAllProducts() {
-    try {
-      const queryString = 'SELECT * FROM products'
-      const data = await db.execute(queryString)
-
-      if (!data) throw new Error()
-
-      return data[0]
-    } catch (error) {
-      return error.message
-    }
-  }
-
-  static async findById(id) {
-    try {
-      const sql = 'SELECT * FROM PRODUCTS WHERE products.id = ?'
-      const data = await db.execute(sql, [id])
-
-      if (!data) throw new Error()
-
-      return data[0][0]
-    } catch (error) {
-      return error.message
-    }
-  }
-
-  static deleteById(id) {}
-}
-
-module.exports = { Product }
+module.exports = Product
