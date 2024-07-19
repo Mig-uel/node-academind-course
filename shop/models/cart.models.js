1e0d2d9a87b5d76d78832e2da0437cd73325948e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-19 15:42:08 -0400

    refactor: Update Cart model to use Sequelize and define primary key
    This commit refactors the Cart model in the cart.models.js file to use Sequelize for database operations. It replaces the previous file system-based approach with a more robust and scalable solution. Additionally, it defines the primary key for the Cart model, ensuring data integrity and efficient querying. This improvement enhances the codebase by leveraging the power of Sequelize and improving the overall performance and maintainability of the application.

--- shop/models/cart.models.js
+++ shop/models/cart.models.js
@@ -1,67 +1,13 @@
-const fs = require('fs')
-const path = require('path')
+const { Sequelize, DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/db.utils')
 
-const pathName = path.join(__dirname, '..', 'data', 'cart.json')
+const Cart = sequelize.define('cart', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+})
 
-class Cart {
-  static addToCart(id, price) {
-    // fetch prev cart
-    fs.readFile(pathName, (err, data) => {
-      let cart = { products: [], totalPrice: 0 }
-
-      if (!err) cart = JSON.parse(data)
-
-      // analyze cart - find existing product
-      const existingProduct = cart.products.find((p) => p.id === id)
-
-      // add new product or increase qty
-      if (existingProduct) {
-        // update cart
-        cart.products = cart.products.map((product) => {
-          if (product.id === existingProduct.id)
-            return { ...existingProduct, qty: existingProduct.qty + 1 }
-          else return product
-        })
-      } else {
-        // update cart
-        cart.products = [...cart.products, { id, qty: 1 }]
-      }
-
-      // update total
-      cart.totalPrice = Number(
-        (Number(cart.totalPrice) + Number(price)).toFixed(2)
-      )
-
-      // save to file
-      fs.writeFile(pathName, JSON.stringify(cart), (err) => console.log(err))
-    })
-  }
-
-  static deleteFromCart(id, price) {
-    fs.readFile(pathName, (err, data) => {
-      if (err) return
-
-      const cart = JSON.parse(data)
-      const product = cart.products.find((p) => p.id === id)
-
-      if (!product) return
-
-      cart.products = cart.products.filter((p) => p.id !== product.id)
-      cart.totalPrice = +(cart.totalPrice - price * product.qty).toFixed(2)
-
-      fs.writeFile(pathName, JSON.stringify(cart), (err) => console.log(err))
-    })
-  }
-
-  static getCart(callback) {
-    fs.readFile(pathName, (err, data) => {
-      const cart = JSON.parse(data)
-
-      if (err) callback(null)
-
-      callback(cart)
-    })
-  }
-}
-
-module.exports = { Cart }
+module.exports = Cart
