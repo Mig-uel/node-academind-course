aefe9741cf438c3b63b363f4288d53139bf4abf3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-19 10:34:29 -0400

    refactor: Update Product model save method to use async/await
    This commit updates the save method in the Product model to use async/await syntax for better readability and maintainability. The method now uses the db.execute function to insert the product data into the database, replacing the previous file-based approach. This improvement enhances the functionality and performance of the shop application by leveraging modern JavaScript features and a more efficient data storage mechanism.

--- shop/models/product.model.js
+++ shop/models/product.model.js
@@ -1,72 +1,50 @@
-const fs = require('fs')
-const path = require('path')
-
-const { v4: uuidv4, v4 } = require('uuid')
+const { db } = require('../utils/db.utils')
 const { Cart } = require('../models/cart.models')
 
-const pathName = path.join(__dirname, '..', 'data', 'products.json')
-
-const getProductsFromFile = (callback) => {
-  fs.readFile(pathName, (error, data) => {
-    if (error) callback([])
-    else {
-      callback(JSON.parse(data))
-    }
-  })
-}
-
 class Product {
-  constructor(title, imageUrl, description, price, id = v4()) {
+  constructor(title, imageUrl, description, price) {
     this.title = title
     this.imageUrl = imageUrl
     this.description = description
     this.price = price
-    this.id = id
   }
 
-  save() {
-    getProductsFromFile((products) => {
-      const existingProduct = products.find((p) => p.id === this.id)
+  async save() {
+    try {
+      const queryString =
+        'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)'
 
-      if (existingProduct) {
-        products = products.map((p) => {
-          if (p.id === existingProduct.id) return this
-          else return p
-        })
-      } else {
-        products.push(this)
-      }
+      const res = await db.execute(queryString, [
+        this.title,
+        this.price,
+        this.imageUrl,
+        this.description,
+      ])
 
-      fs.writeFile(pathName, JSON.stringify(products), (err) =>
-        console.log(err)
-      )
-    })
+      if (!res) throw new Error()
+
+      return res
+    } catch (error) {
+      return error.message
+    }
   }
 
-  static fetchAll(callback) {
-    getProductsFromFile(callback)
+  static async fetchAllProducts() {
+    try {
+      const queryString = 'SELECT * FROM products'
+      const data = await db.execute(queryString)
+
+      if (!data) throw new Error()
+
+      return data[0]
+    } catch (error) {
+      return error.message
+    }
   }
 
-  static findById(id, callback) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id)
+  static findById(id) {}
 
-      callback(product)
-    })
-  }
-
-  static deleteById(id) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id)
-      products = products.filter((p) => p.id !== id)
-
-      fs.writeFile(pathName, JSON.stringify(products), (err) => {
-        if (!err) {
-          Cart.deleteFromCart(id, product.price)
-        }
-      })
-    })
-  }
+  static deleteById(id) {}
 }
 
 module.exports = { Product }
