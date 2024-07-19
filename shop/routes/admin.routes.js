0bb9b3eae420e81b5cf67f49d0de86d72dfc8a69
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-18 20:54:44 -0400

    feat: Add edit and delete functionality for products in admin panel

--- shop/routes/admin.routes.js
+++ shop/routes/admin.routes.js
@@ -4,12 +4,15 @@ const {
   getAddProductForm,
   addProduct,
   adminGetProducts,
+  editProduct,
   getEditProductForm,
+  deleteProduct,
 } = require('../controllers/admin.controller')
 const adminRouter = require('express').Router()
 
 adminRouter.route('/products').get(adminGetProducts)
 adminRouter.route('/products/add').get(getAddProductForm).post(addProduct)
-adminRouter.route('/edit/:id').get(getEditProductForm)
+adminRouter.route('/edit/:id').get(getEditProductForm).patch(editProduct)
+adminRouter.route('/delete').delete(deleteProduct)
 
 module.exports = { adminRouter }
