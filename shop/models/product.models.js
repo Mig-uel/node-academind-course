c901ec47f58b084e1720c64c13c42a57d8a71049
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-20 10:39:23 -0400

    refactor: Update Product constructor in product.models.js
    This commit updates the Product constructor in the product.models.js file. It adds an optional "id" parameter to the constructor, allowing for the creation of a new Product instance with an existing ID. This change improves the flexibility and reusability of the Product class.

--- shop/models/product.models.js
+++ shop/models/product.models.js
@@ -2,20 +2,31 @@ const { ObjectId } = require('mongodb')
 const { connectDB } = require('../utils/db.utils')
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id = null) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
+    this._id = id ? ObjectId.createFromHexString(id) : id
   }
 
   async save() {
     try {
       const db = (await connectDB()).db()
 
+      if (this._id) {
+        await db.collection('products').updateOne(
+          { _id: this._id },
+          {
+            $set: this,
+          }
+        )
+        return
+      }
+
       const res = await db.collection('products').insertOne(this)
 
-      console.log(res)
+      return
     } catch (error) {
       console.log(error)
     }
