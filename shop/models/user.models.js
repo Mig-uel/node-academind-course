0de6cfe0e7ad0c4c85e95800d922df83979c316e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-20 13:19:58 -0400

    refactor: Update User model to use MongoDB and improve code structure

--- shop/models/user.models.js
+++ shop/models/user.models.js
@@ -1,15 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const { sequelize } = require('../utils/db.utils')
+const { connectDB } = require('../utils/db.utils')
+const { ObjectId } = require('mongodb')
 
-const User = sequelize.define('user', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-})
+class User {
+  constructor(username, email, id = null) {
+    this.name = username
+    this.email = email
+    this._id = id ? ObjectId.createFromHexString(id) : id
+  }
 
-module.exports = User
+  async save() {
+    try {
+      const db = (await connectDB()).db()
+
+      if (this._id) {
+        const updatedUser = await db
+          .collection('users')
+          .updateOne({ _id: this._id }, { $set: this })
+
+        return { user: updatedUser, error: null }
+      }
+
+      const user = await db.collection('users').insertOne(this)
+
+      return { user, error: null }
+    } catch (error) {
+      return { user: null, error }
+    }
+  }
+
+  static async findUserById(id) {
+    try {
+      const db = (await connectDB()).db()
+      const objectId = ObjectId.createFromHexString(id)
+
+      if (!ObjectId.isValid(objectId)) throw new Error()
+
+      const user = await db.collection('users').findOne({ _id: objectId })
+
+      console.log(user)
+
+      return { user, error: null }
+    } catch (error) {
+      return { user: null, error }
+    }
+  }
+}
+
+module.exports = { User }
