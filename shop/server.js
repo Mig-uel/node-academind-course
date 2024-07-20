bb8cd9283a34b61bae3e37e8d441ae7a7c3d084f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-20 06:47:44 -0400

    refactor: Remove unnecessary models and relations
    This commit removes the unnecessary models and relations from the server.js file. The Product, User, Cart, and CartItem models, along with their associations, are no longer needed in the codebase. This cleanup improves the organization and maintainability of the application by removing unused code and simplifying the data model.

--- shop/server.js
+++ shop/server.js
@@ -6,11 +6,6 @@ const express = require('express')
 const methodOverride = require('method-override')
 
 // db
-const { sequelize } = require('./utils/db.utils')
-const Product = require('./models/product.models')
-const User = require('./models/user.models')
-const Cart = require('./models/cart.models')
-const CartItem = require('./models/cart-item.models')
 
 // router
 const { adminRouter } = require('./routes/admin.routes')
@@ -45,29 +40,9 @@ app.use(shopRouter)
 // 404
 app.use(use404)
 
-// define relations
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-
-// sequelize sync and start express server
+// start express server
 const init = async () => {
   try {
-    const res = await sequelize.sync()
-
-    if (!res) throw new Error()
-
-    let user = await User.findByPk(1)
-
-    if (!user) {
-      user = await User.create({ name: 'Admin', email: 'admin@email.com' })
-    }
-
-    await user.createCart()
-
     // server
     app.listen(PORT, (req, res) => {
       console.log(`SERVER RUNNING ON PORT: ${PORT}`.green.inverse)
