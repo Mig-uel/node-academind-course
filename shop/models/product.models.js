e00c2bbe831ec1f53e0513ebdaa162deb8c56b04
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-20 09:19:17 -0400

    refactor: Update product.models.js to use MongoDB and improve code structure

--- shop/models/product.models.js
+++ shop/models/product.models.js
@@ -1,29 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const { sequelize } = require('../utils/db.utils')
+const { ObjectId } = require('mongodb')
+const { connectDB } = require('../utils/db.utils')
 
-const Product = sequelize.define('product', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-})
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title
+    this.price = price
+    this.description = description
+    this.imageUrl = imageUrl
+  }
 
-module.exports = Product
+  async save() {
+    try {
+      const db = (await connectDB()).db()
+
+      const res = await db.collection('products').insertOne(this)
+
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async fetchAllProducts() {
+    try {
+      const db = (await connectDB()).db()
+
+      return db.collection('products').find().toArray()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async fetchProductById(id) {
+    try {
+      const db = (await connectDB()).db()
+
+      const product = db
+        .collection('products')
+        .findOne({ _id: ObjectId.createFromHexString(id) })
+
+      return product
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+module.exports = { Product }
