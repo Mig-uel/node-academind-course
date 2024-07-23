d57d83af831d31c6a0ed628eeaa0fd184f8a434d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-23 19:08:00 -0400

    refactor: Update addToCart method in user.models.js
    The code changes in `user.models.js` update the `addToCart` method to handle adding products to the user's cart. The method checks if the product already exists in the cart and increments its quantity if it does. If the product is not in the cart, it adds a new item with a quantity of 1. This change improves the functionality of the application and ensures consistency with recent updates to other models and controllers.

--- shop/models/user.models.js
+++ shop/models/user.models.js
@@ -29,4 +29,22 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
+UserSchema.methods.addToCart = async function (product) {
+  const existingProduct = await this.cart.items.find(
+    (item) => item.productId.toString() === product._id.toString()
+  )
+
+  if (existingProduct) {
+    this.cart.items = this.cart.items.map((item) => {
+      if (item.productId.toString() === existingProduct.productId.toString())
+        return { ...item, qty: item.qty + 1 }
+      else return item
+    })
+  } else {
+    this.cart.items = [...this.cart.items, { productId: product._id, qty: 1 }]
+  }
+
+  await this.save()
+}
+
 module.exports = mongoose.model('User', UserSchema)
