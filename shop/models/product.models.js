ca01f1d41f54a8f0862b8eebcbad2d152a8a8beb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-23 17:48:31 -0400

    refactor: Update Product model to use Mongoose schema

--- shop/models/product.models.js
+++ shop/models/product.models.js
@@ -1,77 +1,27 @@
-const { ObjectId } = require('mongodb')
-const { connectDB } = require('../utils/db.utils')
+const mongoose = require('mongoose')
 
-class Product {
-  constructor(title, price, description, imageUrl, id, userId) {
-    this.title = title
-    this.price = price
-    this.description = description
-    this.imageUrl = imageUrl
-    this._id = id ? ObjectId.createFromHexString(id) : null
-    this._userId = userId || null
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
   }
+)
 
-  async save() {
-    try {
-      const db = (await connectDB()).db()
-
-      if (this._id) {
-        await db.collection('products').updateOne(
-          { _id: this._id },
-          {
-            $set: this,
-          }
-        )
-        return
-      }
-
-      const res = await db.collection('products').insertOne(this)
-
-      return
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  static async fetchAllProducts() {
-    try {
-      const db = (await connectDB()).db()
-
-      return db.collection('products').find().toArray()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  static async fetchProductById(id) {
-    try {
-      const db = (await connectDB()).db()
-
-      const product = db
-        .collection('products')
-        .findOne({ _id: ObjectId.createFromHexString(id) })
-
-      return product
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  static async deleteByProductById(id) {
-    try {
-      const db = (await connectDB()).db()
-
-      const objectId = ObjectId.createFromHexString(id)
-
-      if (!ObjectId.isValid(objectId)) throw new Error()
-
-      const res = await db.collection('products').deleteOne({ _id: objectId })
-
-      return { product: res, error: null }
-    } catch (error) {
-      return { product: null, error }
-    }
-  }
-}
-
-module.exports = { Product }
+module.exports = mongoose.model('Product', ProductSchema)
