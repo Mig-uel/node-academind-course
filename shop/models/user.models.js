baed9e0652c8bf69af48c898d30fb7c30fbf2de0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-23 18:27:32 -0400

    refactor: Update user.models.js to use Mongoose schema
    This commit updates the user.models.js file to use the Mongoose schema for defining the User model. By using the Mongoose schema, we can define the structure and validation rules for the User model fields, such as name and email. This change ensures consistency with recent updates to other models in the project and improves the code structure.

--- shop/models/user.models.js
+++ shop/models/user.models.js
@@ -1,148 +1,31 @@
-const { connectDB } = require('../utils/db.utils')
-const { ObjectId } = require('mongodb')
+const mongoose = require('mongoose')
 
-class User {
-  constructor(username, email, id, cart = { items: [] }) {
-    this.name = username
-    this.email = email
-    this._id = id || null
-    this.cart = cart
-  }
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    cart: {
+      items: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+          },
+          qty: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+)
 
-  async save() {
-    try {
-      const db = (await connectDB()).db()
-
-      if (this._id) {
-        const updatedUser = await db
-          .collection('users')
-          .updateOne({ _id: this._id }, { $set: this })
-
-        return { user: updatedUser, error: null }
-      }
-
-      const user = await db.collection('users').insertOne(this)
-
-      return { user, error: null }
-    } catch (error) {
-      return { user: null, error }
-    }
-  }
-
-  static async findUserById(id) {
-    try {
-      const db = (await connectDB()).db()
-      const objectId = ObjectId.createFromHexString(id)
-
-      if (!ObjectId.isValid(objectId)) throw new Error()
-
-      const user = await db.collection('users').findOne({ _id: objectId })
-
-      return { user, error: null }
-    } catch (error) {
-      return { user: null, error }
-    }
-  }
-
-  async addToCart(product) {
-    const { items } = this.cart
-
-    try {
-      const db = (await connectDB()).db()
-      const existingItem = items.find(
-        (i) => i.productId.toString() === product._id.toString()
-      )
-
-      if (existingItem) {
-        this.cart.items = items.map((i) => {
-          if (i.productId === existingItem.productId)
-            return { ...existingItem, qty: existingItem.qty + 1 }
-          else return i
-        })
-      } else {
-        this.cart.items = [...items, { productId: product._id, qty: 1 }]
-      }
-
-      const addItem = await db
-        .collection('users')
-        .updateOne({ _id: this._id }, { $set: { cart: this.cart } })
-
-      return addItem
-    } catch (error) {}
-  }
-
-  async getCart() {
-    try {
-      const db = (await connectDB()).db()
-
-      const cartItemIdArray = this.cart.items.map((i) => i.productId)
-
-      const cart = await db
-        .collection('products')
-        .find({ _id: { $in: cartItemIdArray } })
-        .toArray()
-
-      const fullCart = cart.map((i) => ({
-        ...i,
-        qty: this.cart.items.find(
-          (p) => i._id.toString() === p.productId.toString()
-        ).qty,
-      }))
-
-      return fullCart
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async deleteItemFromCart(id) {
-    try {
-      this.cart.items = this.cart.items.filter(
-        (item) => item.productId.toString() !== id.toString()
-      )
-
-      const db = (await connectDB()).db()
-      await db
-        .collection('users')
-        .updateOne({ _id: this._id }, { $set: { cart: this.cart } })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async addOrder() {
-    try {
-      const db = (await connectDB()).db()
-      const cart = await this.getCart()
-
-      const order = await db.collection('orders').insertOne({
-        items: cart,
-        user: { _id: this._id, name: this.name },
-      })
-
-      // update local and server cart
-      await db
-        .collection('users')
-        .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } })
-      this.cart = { items: [] }
-
-      return
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async getOrders() {
-    try {
-      const db = (await connectDB()).db()
-      return await db
-        .collection('orders')
-        .find({ 'user._id': this._id })
-        .toArray()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-}
-
-module.exports = { User }
+module.exports = mongoose.model('User', UserSchema)
