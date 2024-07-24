5cabbc6b380182f6bc3afac53a4cabf804fabe8b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-24 19:08:31 -0400

    refactor: Update error controller to include isAuthenticated flag

--- shop/controllers/error.controller.js
+++ shop/controllers/error.controller.js
@@ -1,4 +1,7 @@
 const use404 = (req, res, next) =>
-  res.status(404).render('404', { docTitle: 'Page Not Found' })
+  res.status(404).render('404', {
+    docTitle: 'Page Not Found',
+    isAuthenticated: req.cookies.loggedIn,
+  })
 
 module.exports = { use404 }
