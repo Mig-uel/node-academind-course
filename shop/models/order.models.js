5c659674bea28617b3dfc082977975369ce526d7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-24 16:20:09 -0400

    refactor: Add Order model to handle user orders
    This commit adds the Order model to the shop/models directory. The Order model includes fields for products (an array of product IDs and quantities) and the user ID. This change allows the application to handle user orders and improves the functionality of the application.

---
+++ shop/models/order.models.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        qty: { type: Number, required: true },
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Order', OrderSchema)
