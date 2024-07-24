9221e6c033f8c954990bc9a0d892ff48682048a3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-24 19:08:25 -0400

    refactor: Add login and logout functionality to auth.controller.js
    Add the login and logout functionality to the auth.controller.js file. This includes the login and logout methods, which handle the logic for setting and clearing the loggedIn cookie. These changes enhance the authentication capabilities of the application and improve the user experience by allowing users to log in and out.

--- shop/controllers/auth.controller.js
+++ shop/controllers/auth.controller.js
@@ -1,8 +1,33 @@
 const getLogin = async (req, res) => {
+  const loggedIn = req.cookies.loggedIn
+
   return res.render('auth/login', {
     path: '/login',
     docTitle: 'Login',
+    isAuthenticated: loggedIn,
   })
 }
 
-module.exports = { getLogin }
+const login = async (req, res) => {
+  try {
+    res.cookie('loggedIn', 'true')
+
+    return res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const logout = async (req, res) => {
+  try {
+    const cookie = req.cookies.loggedIn
+    res.cookie('loggedIn', '', { maxAge: Date.now() })
+
+    console.log(cookie)
+    return res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { getLogin, login, logout }
