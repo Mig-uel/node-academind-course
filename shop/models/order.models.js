1d9ee13c01c88dc68e5719097891bd77a3cfbd4c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-24 17:41:51 -0400

    refactor: Update Order model in order.models.js
    Update the Order model in order.models.js to include additional fields for product details such as title, price, description, and imageUrl. This change enhances the functionality of the application by providing more comprehensive information about the products in an order.

--- shop/models/order.models.js
+++ shop/models/order.models.js
@@ -4,11 +4,10 @@ const OrderSchema = new mongoose.Schema(
   {
     products: [
       {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true,
-        },
+        title: { type: String, required: true },
+        price: { type: Number, required: true },
+        description: { type: String, required: true },
+        imageUrl: { type: String, requierd: true },
         qty: { type: Number, required: true },
       },
     ],
