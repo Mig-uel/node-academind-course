fdd4c23b8f154f544b401af481651fef278afb14
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 18:38:34 -0400

    feat: Update email.utils.js to include access token for OAuth2 authentication
    The email.utils.js file has been modified to include the access token for OAuth2 authentication. This change enables the sending of emails using nodemailer with the Gmail service. The scope and accessToken properties have been added to the transporterOptions object. This commit builds upon previous commits that added email sending functionality and updated the package dependencies.

--- shop/utils/email.utils.js
+++ shop/utils/email.utils.js
@@ -2,6 +2,7 @@ const nodemailer = require('nodemailer')
 
 const transporterOptions = {
   service: 'gmail',
+  scope: 'https://mail.google.com',
   auth: {
     type: 'OAuth2',
     user: process.env.GMAIL_USERNAME,
@@ -9,17 +10,18 @@ const transporterOptions = {
     clientId: process.env.OAUTH_CLIENT_ID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.OAUTH_ACCESS_TOKEN,
   },
 }
 
-const sendMail = async (email) => {
+const sendMail = async (toEmail, subject, text) => {
   const transporter = nodemailer.createTransport(transporterOptions)
 
   let mailOptions = {
-    from: 'tomerpacific@gmail.com',
-    to: 'miguelcastillo1000@gmail.com',
-    subject: 'Nodemailer Project',
-    text: 'Hi from your nodemailer project',
+    from: 'admin@gmail.com',
+    to: toEmail,
+    subject,
+    text,
   }
 
   transporter.sendMail(mailOptions, (err, data) => {
