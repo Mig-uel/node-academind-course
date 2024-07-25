d05629147016455f528d160252ee5b5f1d9e2b2d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 10:57:43 -0400

    refactor: Add getSignUp and signup routes to auth.routes.js
    The auth.routes.js file has been updated to include the getSignUp and signup routes. These routes handle the rendering of the signup page and the signup form submission, respectively. This change enhances the authentication functionality of the application.

--- shop/routes/auth.routes.js
+++ shop/routes/auth.routes.js
@@ -1,11 +1,18 @@
 const { Router } = require('express')
 
 // controllers
-const { getLogin, login, logout } = require('../controllers/auth.controller')
+const {
+  getLogin,
+  login,
+  logout,
+  getSignUp,
+  signup,
+} = require('../controllers/auth.controller')
 
 const authRouter = Router()
 
 authRouter.route('/login').get(getLogin).post(login)
 authRouter.route('/logout').get(logout)
+authRouter.route('/signup').get(getSignUp).post(signup)
 
 module.exports = { authRouter }
