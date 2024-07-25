e2e7618a72ea567312ffe4544d598899d2d66bed
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 16:05:57 -0400

    refactor: Update server.js to remove unused middleware and improve code organization
    The server.js file has been updated to remove unused middleware such as csrf-csrf and cookie-parser. Additionally, the code organization has been improved by removing redundant middleware calls and reordering the middleware setup. This change enhances the efficiency and readability of the application.

--- shop/server.js
+++ shop/server.js
@@ -10,9 +10,7 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
-const { doubleCsrf: csrf } = require('csrf-csrf')
-const cookieParser = require('cookie-parser')
-const { hydrateUser, csrfMiddleware } = require('./middleware/auth.middleware')
+const { hydrateUser } = require('./middleware/auth.middleware')
 
 // db
 const { db } = require('./utils/db.utils')
@@ -31,19 +29,21 @@ const store = new MongoDBStore({
   uri: process.env.MONGO_URI,
   collection: 'sessions',
 })
-const csrfProtection = csrf({
-  getSecret: () => process.env.CSRF_SECRET,
-  getTokenFromRequest: (req) => req.body._csrf,
-})
-db()
 
 // express setup
 app.set('view engine', 'ejs') // set view engine
 app.set('views', 'views') // already default, just example
 
 // middleware
-app.use(cookieParser(process.env.CSRF_SECRET))
-app.use(csrfProtection.doubleCsrfProtection)
+app.use(express.static('public')) // server static files/grant access (public folder)
+app.use((req, res, next) => {
+  console.log(`[${req.method}] - ${req.url} - ${res.statusCode}`.yellow)
+  next()
+}) // method - url - status
+app.use(express.urlencoded({ extended: true })) // parse form data
+app.use(express.json()) // parse json data
+app.use(methodOverride('_method'))
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -56,23 +56,16 @@ app.use(
   })
 )
 app.use(hydrateUser)
-app.use(csrfMiddleware)
-app.use(express.static('public')) // server static files/grant access (public folder)
-app.use(methodOverride('_method'))
-app.use((req, res, next) => {
-  console.log(`[${req.method}] - ${req.url} - ${res.statusCode}`.yellow)
-  next()
-}) // method - url - status
-app.use(express.urlencoded({ extended: true })) // parse form data
-app.use(express.json()) // parse json data
 
-// routes\
+// routes
 app.use('/auth', authRouter)
 app.use('/admin', adminRouter)
 app.use('/', shopRouter)
 
 // 404
 app.use(use404)
+
+db()
 
 mongoose.connection.once('open', () => {
   // start express server
