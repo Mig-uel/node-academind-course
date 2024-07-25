67cdd55c3c1a6810b6a2e8d3e75a62d37059acf8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 10:19:13 -0400

    refactor: Add hydrateUser middleware and update isAuthenticated middleware
    The auth.middleware.js file has been updated to include the hydrateUser middleware, which hydrates the user object from the session data and attaches it to the request object. Additionally, the isAuthenticated middleware has been updated to use session-based authentication. These changes improve the authentication functionality of the application.

--- shop/middleware/auth.middleware.js
+++ shop/middleware/auth.middleware.js
@@ -1,3 +1,5 @@
+const User = require('../models/user.models')
+
 const isAuthenticated = async (req, res, next) => {
   if (req.session.user) return next()
   else {
@@ -5,4 +7,12 @@ const isAuthenticated = async (req, res, next) => {
   }
 }
 
-module.exports = { isAuthenticated }
+const hydrateUser = async (req, res, next) => {
+  if (req.session.user) {
+    req.session.user = new User().init(req.session.user)
+  }
+
+  next()
+}
+
+module.exports = { isAuthenticated, hydrateUser }
