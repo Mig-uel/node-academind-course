2b8ddf5f99564badab7e07e279d3b95058b1b035
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 09:50:47 -0400

    refactor: Update logout route to use GET instead of POST
    The logout route in the auth.routes.js file has been updated to use the GET method instead of POST. This change aligns with the HTTP semantics and improves the consistency of the application's routing.

--- shop/routes/auth.routes.js
+++ shop/routes/auth.routes.js
@@ -6,6 +6,6 @@ const { getLogin, login, logout } = require('../controllers/auth.controller')
 const authRouter = Router()
 
 authRouter.route('/login').get(getLogin).post(login)
-authRouter.route('/logout').post(logout)
+authRouter.route('/logout').get(logout)
 
 module.exports = { authRouter }
