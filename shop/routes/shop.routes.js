49628a39acb276b46c67550405b1b6322fc9f12e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 10:03:42 -0400

    refactor: Update shop.routes.js to use isAuthenticated middleware
    The shop.routes.js file has been updated to include the isAuthenticated middleware for the '/cart' and '/checkout' routes. This change ensures that only authenticated users can access these routes, improving the security of the application.

--- shop/routes/shop.routes.js
+++ shop/routes/shop.routes.js
@@ -1,3 +1,6 @@
+// middleware
+const { isAuthenticated } = require('../middleware/auth.middleware')
+
 // controllers
 const shopRouter = require('express').Router()
 const {
@@ -15,8 +18,12 @@ const {
 shopRouter.route('/').get(getHome)
 shopRouter.route('/products').get(getProducts)
 shopRouter.route('/products/:id').get(getProduct)
-shopRouter.route('/cart').get(getCart).post(addToCart).delete(removeFromCart)
-shopRouter.route('/checkout').get(getCheckout)
-shopRouter.route('/orders').get(getOrders).post(addOrder)
+shopRouter
+  .route('/cart')
+  .get(isAuthenticated, getCart)
+  .post(addToCart)
+  .delete(removeFromCart)
+shopRouter.route('/checkout').get(isAuthenticated, getCheckout)
+shopRouter.route('/orders').get(isAuthenticated, getOrders).post(addOrder)
 
 module.exports = { shopRouter }
