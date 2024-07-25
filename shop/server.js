d34d20d64f5a961b0aa90deb3920e3fcee6e029e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-24 20:49:05 -0400

    refactor: Update server.js to use express-session for session management
    The server.js file has been updated to replace the cookie-parser middleware with express-session for session management. This change enhances the security and user experience of the application by providing more robust session handling capabilities.

--- shop/server.js
+++ shop/server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 // db
 const { db } = require('./utils/db.utils')
@@ -30,7 +30,17 @@ app.set('view engine', 'ejs') // set view engine
 app.set('views', 'views') // already default, just example
 
 // middleware
-app.use(cookieParser())
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: 'strict',
+    },
+  })
+)
 app.use(async (req, res, next) => {
   const user = await User.findById('66a02ed53e6b9281c2e26289')
 
