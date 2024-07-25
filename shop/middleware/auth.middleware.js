b83013cb5127bc9d8d29b5985c31fd735390aab9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-25 12:49:18 -0400

    refactor: Update auth.middleware.js to redirect unauthenticated users to login

--- shop/middleware/auth.middleware.js
+++ shop/middleware/auth.middleware.js
@@ -2,9 +2,8 @@ const User = require('../models/user.models')
 
 const isAuthenticated = async (req, res, next) => {
   if (req.session.user) return next()
-  else {
-    return res.redirect('/auth/login')
-  }
+  
+  return res.redirect('/auth/login')
 }
 
 const hydrateUser = async (req, res, next) => {
