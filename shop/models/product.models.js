01d56ad3740c2485486abbb736a237019faaad98
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-07-31 17:46:02 -0400

    refactor: Remove deleted products from user's cart
    The code changes in the `product.models.js` file remove deleted products from the user's cart. The `deleteOne` pre-hook function checks for users who have the deleted product in their cart and removes it from their cart items. This ensures that the user's cart remains updated and consistent with the available products.
    
    Remove deleted products from user's cart in product.models.js

--- shop/models/product.models.js
+++ shop/models/product.models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const User = require('./user.models')
 
 const ProductSchema = new mongoose.Schema(
   {
@@ -28,5 +29,20 @@ const ProductSchema = new mongoose.Schema(
     timestamps: true,
   }
 )
+
+ProductSchema.pre('deleteOne', async function () {
+  const productId = this._conditions._id
+
+  const users = await User.find({ 'cart.items.productId': productId })
+
+  if (!users) return
+
+  users.forEach(async (user) => {
+    user.cart.items = user.cart.items.filter(
+      (item) => item.productId.toString() !== productId.toString()
+    )
+    await user.save()
+  })
+})
 
 module.exports = mongoose.model('Product', ProductSchema)
