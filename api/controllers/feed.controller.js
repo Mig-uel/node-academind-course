b0a67a12d4fb3e536b83e517e06af782293ef643
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-04 17:44:18 -0400

    feat: Add functionality to get and add posts in feed controller

--- api/controllers/feed.controller.js
+++ api/controllers/feed.controller.js
@@ -1,3 +1,23 @@
+/**
+ * @method GET
+ * @route /feed/posts
+ */
 exports.getPosts = (req, res, next) => {
-  return res.status(200).json({ message: 'ok' })
+  return res.status(200).json({
+    title: 'First Post',
+    content: 'This is the first post!',
+  })
+}
+
+/**
+ * @method POST
+ * @route /feed/posts
+ */
+exports.addPost = (req, res, next) => {
+  const { title, content } = req.body
+  const post = { title, content, id: new Date().toISOString() }
+
+  // create post in database
+
+  return res.status(201).json({ message: 'Post created', post })
 }
