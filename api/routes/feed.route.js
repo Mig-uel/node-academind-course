15347a6ed81343109c71d2c5f1baf9bca9bda182
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-04 17:44:27 -0400

    feat: Add POST route for adding posts in feed functionality

--- api/routes/feed.route.js
+++ api/routes/feed.route.js
@@ -1,15 +1,12 @@
 const { Router } = require('express')
 
 // feed controllers
-const { getPosts } = require('../controllers/feed.controller')
+const { getPosts, addPost } = require('../controllers/feed.controller')
 
 // init router obj
 const router = Router()
 
-/**
- * @method GET
- * @route /feed/posts
- */
 router.get('/posts', getPosts)
+router.post('/posts', addPost)
 
 module.exports = router
