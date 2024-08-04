aa6bcf3aae93d49b868aab0ed83c62789c53595b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-04 17:24:53 -0400

    feat: Add feed router to express server
    The code changes in `api/app.js` add the feed router to the express server. This allows the server to handle requests related to the feed functionality. This feature enhancement improves the modularity and organization of the application's routing system.

--- api/app.js
+++ api/app.js
@@ -3,9 +3,15 @@ const express = require('express')
 const PORT = 8080
 const app = express()
 
+// routers
+const feedRouter = require('./routes/feed.route')
+
 // body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.use('/feed', feedRouter)
 
 app.listen(PORT, () => {
   console.log('Server is running...')
