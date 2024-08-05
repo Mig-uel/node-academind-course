d4d9663ac9cc71df1b124bf60ca5464049d12759
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-05 19:46:30 -0400

    refactor: Add errorHandler middleware to handle errors in the backend application

---
+++ social-network/backend/middleware/errorHandler.middleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (error, req, res, next) => {
+  const status = error.statusCode || 500
+  const { message } = error
+  const stack = process.env.NODE_ENV === 'dev' ? error.stack : '🥞'
+
+  return res.status(status).json({ status, message, stack })
+}
+
+module.exports = { errorHandler }
