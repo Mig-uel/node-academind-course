599aa80f2ecad1afd1bbf9e46eb0fa9ca347aa34
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-05 18:14:02 -0400

    refactor: Add validation for post title and content in addPost route

--- social-network/backend/routes/feed.route.js
+++ social-network/backend/routes/feed.route.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const { body } = require('express-validator')
 
 // feed controllers
 const { getPosts, addPost } = require('../controllers/feed.controller')
@@ -6,7 +7,17 @@ const { getPosts, addPost } = require('../controllers/feed.controller')
 // init router obj
 const router = Router()
 
+// GET /feed/posts
 router.get('/posts', getPosts)
-router.post('/posts', addPost)
+
+// POST /feed/posts
+router.post(
+  '/posts',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  addPost
+)
 
 module.exports = router
