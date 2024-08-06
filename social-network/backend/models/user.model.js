ac7b2f63f6bb5413e4ee154b8b62e2287e88caab
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-06 18:20:00 -0400

    feat: Add User model for backend
    This commit adds the User model to the backend. The User model includes fields for email, password, name, status, and posts. This feature enhances the functionality of the backend application by providing a data structure to store user information.

---
+++ social-network/backend/models/user.model.js
@@ -0,0 +1,19 @@
+const { Schema, default: mongoose } = require('mongoose')
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    status: { type: String, default: 'I am new!' },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('User', UserSchema)
