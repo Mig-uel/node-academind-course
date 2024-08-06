ef99076a31586b4051271d032dd88b54c69c8b33
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-05 20:12:37 -0400

    refactor: Use asyncHandler utility function in addPost route
    The code changes in `feed.controller.js` refactor the `addPost` route to use the `asyncHandler` utility function from `asyncHandler.utils.js`. This refactor improves the error handling capabilities of the backend application.

--- social-network/backend/controllers/feed.controller.js
+++ social-network/backend/controllers/feed.controller.js
@@ -1,3 +1,4 @@
+const { asyncHandler } = require('../utils/asyncHandler.utils')
 const { validationResult } = require('express-validator')
 const Post = require('../models/post.model')
 
@@ -26,32 +27,28 @@ exports.getPosts = (req, res, next) => {
  * @method POST
  * @route /feed/posts
  */
-exports.addPost = async (req, res, next) => {
-  try {
-    const errors = validationResult(req)
+exports.addPost = asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req)
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        message: 'Invalid fields, please try again.',
-        errors: errors.array(),
-      })
-    }
+  if (!errors.isEmpty()) {
+    const error = new Error('Invalid or missing fields, please try again.')
+    error.statusCode = 422
 
-    const { title, content } = req.body
-
-    // create post in database
-    const post = new Post({
-      title,
-      content,
-      creator: { name: 'Miguel' },
-      imageUrl: 'images/duckies.jpg',
-    })
-    await post.save()
-
-    if (!post) throw new Error()
-
-    return res.status(201).json({ message: 'Post created', post })
-  } catch (error) {
-    console.log(error)
+    throw error
   }
-}
+
+  const { title, content } = req.body
+
+  // create post in database
+  const post = new Post({
+    title,
+    content,
+    creator: { name: 'Miguel' },
+    imageUrl: 'images/duckies.jpg',
+  })
+  await post.save()
+
+  if (!post) throw new Error()
+
+  return res.status(201).json({ message: 'Post created', post })
+})
