73c5b11c1180184790a0e3cb7c3e1270a137cf18
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-06 20:07:00 -0400

    feat: Add login functionality to auth controller
    This commit adds the login functionality to the `auth.controller.js` file. The `login` function handles the authentication of a user by checking the email and password, generating a JWT token, and returning it along with the user ID. This feature enhances the functionality of the backend application by providing the ability for users to log in securely.

--- social-network/backend/controllers/auth.controller.js
+++ social-network/backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { asyncHandler } = require('../utils/asyncHandler.utils')
 const { validationResult } = require('express-validator')
 const { throwError } = require('../utils/throwError.utils')
@@ -20,4 +21,25 @@ exports.signup = asyncHandler(async (req, res, next) => {
   await user.save()
 
   return res.status(201).json({ message: 'User created.', userId: user._id })
+})
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user) throwError('Incorrect email or password.', 401)
+
+  const comparePassword = await bcrypt.compare(password, user.password)
+
+  if (!comparePassword) throwError('Incorrect email or password.', 401)
+
+  // generate new jwt token
+  const token = jwt.sign(
+    { email, userId: user._id.toString() },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  )
+
+  return res.status(200).json({ token, userId: user._id.toString() })
 })
