6103e327ca09e9e1aa1c00130254929db16e36b7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-07 19:29:43 -0400

    feat: Add authorization header to fetch request in SinglePost.js

--- social-network/frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ social-network/frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -17,7 +17,11 @@ class SinglePost extends Component {
   componentDidMount() {
     /** USE CUSTOM HOC TO WRAP THIS COMPONENT */
     const postId = this.props.params.postId
-    fetch(`http://localhost:8080/feed/posts/${postId}`)
+    fetch(`http://localhost:8080/feed/posts/${postId}`, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status')
