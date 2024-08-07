a1c3e5e7d91ed5ff5be1c381b41cf12dec91115c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-07 19:29:24 -0400

    feat: Add comments

--- social-network/backend/middleware/auth.middleware.js
+++ social-network/backend/middleware/auth.middleware.js
@@ -3,9 +3,11 @@ const { asyncHandler } = require('../utils/asyncHandler.utils')
 const { throwError } = require('../utils/throwError.utils')
 
 exports.isAuth = asyncHandler(async (req, res, next) => {
+  // retrieve 'authorization' header from request
   const authHeader = req.get('Authorization')
   if (!authHeader) throwError('Unauthorized.', 401)
 
+  // get token from header
   const token = authHeader.split(' ')[1]
 
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
