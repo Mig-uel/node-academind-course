4140ab6856e95d6af98afe3d345c6ed30700b8ee
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-07 22:26:40 -0400

    refactor: Update socket server initialization in app.js
    This commit updates the initialization of the socket server in the `app.js` file. It replaces the previous usage of the `socket.io` function with the `Server` class from the `socket.io` library. The new approach allows for better configuration of the socket server, including enabling CORS for the origin `http://localhost:3000` and specifying the allowed methods as `GET` and `POST`. This improvement enhances the functionality and security of the backend application.

--- social-network/backend/app.js
+++ social-network/backend/app.js
@@ -49,16 +49,7 @@ mongoose.connection.once('open', () => {
   })
 
   // create socket server
-  const { Server } = require('socket.io')
-
-  const io = new Server(httpServer, {
-    cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] },
-  })
-
-  io.on('connection', (socket) => {
-    console.log(`SOCKET.IO: ${socket.id} CONNECTED`)
-    console.log(' x - - - - - - - - - - - - x')
-  })
+  const io = require('./socket').init(httpServer)
 })
 
 mongoose.connection.on('error', (error) => {
