a4a1c12b0c61ef3c8e94ee999757aedb34a906d9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-08 02:22:15 -0400

    refactor: Update app.js to remove unused routers and routes
    The code changes in `app.js` remove the unused routers and routes for `feed`, `auth`, and `status`. These changes clean up the codebase by removing unnecessary code, improving maintainability and reducing potential confusion for future developers.

--- social-network/backend/app.js
+++ social-network/backend/app.js
@@ -9,11 +9,6 @@ const { connectToDatabase } = require('./utils/db.utils')
 const port = process.env.PORT || 4000
 const app = express()
 
-// routers
-const feedRouter = require('./routes/feed.route')
-const authRouter = require('./routes/auth.route')
-const statusRouter = require('./routes/status.route')
-
 // serve static files
 app.use('/images', express.static('images')) // virtual path: /images
 
@@ -29,11 +24,6 @@ app.use((req, res, next) => {
   next()
 })
 
-// routes
-app.use('/feed', feedRouter)
-app.use('/auth', authRouter)
-app.use('/status', statusRouter)
-
 // error handler
 app.use(errorHandler)
 
@@ -42,17 +32,9 @@ connectToDatabase()
 
 mongoose.connection.once('open', () => {
   // start express server
-  const httpServer = app.listen(port, async () => {
+  app.listen(port, async () => {
     console.log(' x - - - - - - - - - - - - x')
     console.log(`SERVER RUNNING ON PORT: ${port}`)
-    console.log(' x - - - - - - - - - - - - x')
-  })
-
-  // create socket server
-  const io = require('./socket').init(httpServer)
-
-  io.on('connection', (socket) => {
-    console.log(`SOCKET.IO: ${socket.id} CONNECTED`)
     console.log(' x - - - - - - - - - - - - x')
   })
 })
