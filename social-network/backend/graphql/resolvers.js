e87d1016b9d60ff3a6fd6c59e791000c6ea9ed00
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-08 15:39:43 -0400

    refactor: Add validation for signup inputs
    This code change adds validation for the signup inputs in the `signup` resolver function. It uses the `validator` library to check if the name is empty, if the email is invalid, and if the password is too short. If any validation errors are found, an error is thrown with the corresponding error messages. This enhancement ensures that only valid inputs are accepted for the signup process, improving the overall data integrity and user experience.

--- social-network/backend/graphql/resolvers.js
+++ social-network/backend/graphql/resolvers.js
@@ -1,10 +1,29 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 const User = require('../models/user.model')
 
 // the root provides a resolver function for each API endpoint
 exports.root = {
   async signup(args, req) {
     const { email, password, name } = args.userInput
+
+    // validation
+    const errors = []
+    if (validator.isEmpty(name))
+      errors.push({ type: 'name', message: 'Name is empty.' })
+    if (!validator.isEmail(email))
+      errors.push({ type: 'email', message: 'Email is invalid.' })
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    )
+      errors.push({ type: 'password', message: 'Password too short.' })
+    if (errors.length) {
+      const error = new Error('Invalid input.')
+      error.data = errors
+      error.code = 422
+      throw error
+    }
 
     const existingUser = await User.findOne({ email })
     if (existingUser) {
