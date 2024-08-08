61fbe593abe97b3a96381a91103794f718352839
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-08 18:52:51 -0400

    refactor: Update isAuth middleware to handle missing authorization header
    This code change updates the isAuth middleware in the auth.middleware.js file to handle cases where the 'Authorization' header is missing. If the header is not present, the req.isAuth flag is set to false and the next middleware is called. This improvement ensures that the authentication process gracefully handles missing authorization headers, enhancing the overall reliability and user experience of the application.

--- social-network/backend/middleware/auth.middleware.js
+++ social-network/backend/middleware/auth.middleware.js
@@ -1,20 +1,26 @@
 const jwt = require('jsonwebtoken')
 const { asyncHandler } = require('../utils/asyncHandler.utils')
-const { throwError } = require('../utils/throwError.utils')
 
 exports.isAuth = asyncHandler(async (req, res, next) => {
   // retrieve 'authorization' header from request
   const authHeader = req.get('Authorization')
-  if (!authHeader) throwError('Unauthorized.', 401)
+  if (!authHeader) {
+    req.isAuth = false
+    next()
+  }
 
   // get token from header
   const token = authHeader.split(' ')[1]
 
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
 
-  if (!decodedToken) throwError('Unauthorized.', 401)
+  if (!decodedToken) {
+    req.isAuth = false
+    next()
+  }
 
   req.userId = decodedToken.userId
+  req.isAuth = true
 
   next()
 })
