babb73756d9c97e49a55e83a462202a040a57be3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-10 15:54:04 -0400

    refactor: Update app.ts to use Deno server for handling requests

--- deno/app.ts
+++ deno/app.ts
@@ -1,3 +1,12 @@
-const server = Deno.serve({ port: 3000 }, (_req) => {
-  return new Response('Hello, world!')
-})
+const port = 3000
+
+const handler = (req: Request): Response => {
+  const body = `Your user agent is: \n\n${
+    req.headers.get('user-agent') ?? 'Unknown'
+  }`
+
+  console.log('Hello')
+  return new Response(body, { status: 200 })
+}
+
+const server = Deno.serve({ port }, handler)
