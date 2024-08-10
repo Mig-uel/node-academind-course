a3ba6533015adf96e6d77dd2c7fe65a184cd06d9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-10 01:59:09 -0400

    feat: Add text input value to create new todo in todos route

--- node-typescript/routes/todos.ts
+++ node-typescript/routes/todos.ts
@@ -10,11 +10,13 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+  const { text } = req.body
 
-  
-  const newTodo: ITodo = { id: '1', text: 'neva' }
+  const newTodo: ITodo = { id: '1', text }
 
   todos.push(newTodo)
+
+  return res.status(201).json({message: 'Todo added!'})
 })
 
 export default router
