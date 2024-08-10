e1d38145e3c7c1f53f3cb0c490d80d8205e47e87
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-10 01:54:10 -0400

    refactor: Update app.ts to use Express for web server
    This code change updates the app.ts file to use the Express framework for building a web server. The previous implementation used a custom interface and a Promise, which has been replaced with the express module. This refactor improves the code's maintainability and readability by leveraging the widely-used Express framework for handling HTTP requests and responses.

--- node-typescript/app.ts
+++ node-typescript/app.ts
@@ -1,3 +1,13 @@
-const express = require('express')
+import express from 'express'
+
+// routes
+import todosRouter from './routes/todos'
 
 const app = express()
+const port = 8080
+
+app.use('/todos', todosRouter)
+
+app.listen(port, () => {
+  console.log(`SERVER RUNNING ON PORT: ${port}`)
+})
