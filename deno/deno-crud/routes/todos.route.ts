e241229996f122dd85fd4325fefea85391504445
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-11 21:24:02 -0400

    feat: Add CRUD routes for todos
    This code change adds CRUD routes for todos in the todos.route.ts file. It includes GET, POST, PATCH, and DELETE routes for managing todo items. This update enhances the functionality of the application by allowing users to perform basic CRUD operations on todos.

---
+++ deno/deno-crud/routes/todos.route.ts
@@ -0,0 +1,13 @@
+import { Router } from 'https://deno.land/x/oak/mod.ts'
+
+const router = new Router()
+
+router.get('/', (ctx) => {})
+
+router.post('/', (ctx) => {})
+
+router.patch('/:id', (ctx) => {})
+
+router.delete('/:id', (ctx) => {})
+
+export default router
