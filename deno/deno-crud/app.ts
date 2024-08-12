55f950d720410e017cce9c5133a0ea1b5bcee30e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-11 21:30:54 -0400

    refactor: Add todos CRUD routes using Oak
    This code change adds CRUD routes for todos using Oak in the todos.route.ts file. It includes GET, POST, PATCH, and DELETE routes for managing todo items. This update enhances the functionality of the application by allowing users to perform basic CRUD operations on todos.

--- deno/deno-crud/app.ts
+++ deno/deno-crud/app.ts
@@ -1,10 +1,17 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 
+// router
+import todosRouter from './routes/todos.route.ts'
+
 const app = new Application()
 
 // middleware
 // app.use((ctx) => {
 //   ctx.response.body = 'Hello, world!'
 // })
+
+// routes
+app.use(todosRouter.routes)
+app.use(todosRouter.allowedMethods())
 
 await app.listen({ port: 3000 })
