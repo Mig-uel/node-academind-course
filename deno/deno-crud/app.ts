74559fb6717424b43543016c4a070d9016508dc6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-12 19:20:55 -0400

    refactor: Update app.ts to use Oak for handling requests

--- deno/deno-crud/app.ts
+++ deno/deno-crud/app.ts
@@ -5,13 +5,8 @@ import todosRouter from './routes/todos.route.ts'
 
 const app = new Application()
 
-// middleware
-// app.use((ctx) => {
-//   ctx.response.body = 'Hello, world!'
-// })
-
 // routes
-app.use(todosRouter.routes)
+app.use(todosRouter.routes())
 app.use(todosRouter.allowedMethods())
 
 await app.listen({ port: 3000 })
