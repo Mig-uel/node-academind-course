60dd71205ccf341b2db9d9323179c557e1485d42
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-11 21:14:11 -0400

    refactor: Update todos.route.js to include PATCH and DELETE routes
    This code change adds PATCH and DELETE routes to the todos.route.js file, allowing users to update and delete todo items. The PATCH route updates the text of a specific todo item, while the DELETE route removes a todo item from the list. This enhancement improves the functionality of the application by enabling users to perform more actions on their todos.

--- deno/node-crud/routes/todos.route.js
+++ deno/node-crud/routes/todos.route.js
@@ -12,11 +12,26 @@ router.post('/', (req, res) => {
 
   todos.push({ id: new Date().getTime(), text })
 
-  return res.status(200).json({ message: 'todo added', todos })
+  return res.status(201).json({ message: 'todo added', todos })
 })
 
-router.patch('/:id', (req, res) => {})
+router.patch('/:id', (req, res) => {
+  const { id } = req.params
+  const { text } = req.body
 
-router.delete('/:id', (req, res) => {})
+  const index = todos.findIndex((todo) => todo.id === id)
+
+  todos[index] = { ...todos[index], text }
+
+  return res.status(200).json({ message: 'todo updated', todos })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  todos = todos.filter((todo) => todo.id !== id)
+
+  return res.status(200).json({ message: 'todo deleted', todos })
+})
 
 module.exports = router
